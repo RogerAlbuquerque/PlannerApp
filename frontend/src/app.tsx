da29b738c3b406a66368877d4cb1647031e9e1c4
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-07-10 18:26:55 -0300

    feat: created confirm trip modal

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,10 @@
-import { MapPin, Calendar, ArrowRight, Settings2, UserRoundPlus, X, AtSign, Plus } from "lucide-react";
+import { MapPin, Calendar, ArrowRight, Settings2, UserRoundPlus, X, AtSign, Plus, User } from "lucide-react";
 import { FormEvent, useState } from "react";
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
   const [emailsToInvite, setEmailsToInvite] = useState([
     'roger@gmail.com'
   ]);
@@ -23,6 +24,15 @@ export function App() {
   function closeGuestsModal() {
     setIsGuestsModalOpen(false)
   }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true)
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false)
+  }
+
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -103,12 +113,15 @@ export function App() {
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
               <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <span className="text-zinc-400 text-lg flex-1">Who will be on the trip?</span>
+                {emailsToInvite.length > 0 ? 
+                  (<span className="text-zinc-100 text-lg flex-1">{emailsToInvite.length} {emailsToInvite.length <= 1 ?'person' : 'people'} invited</span>) 
+                    :
+                  (<span className="text-zinc-400 text-lg flex-1">Who will be on the trip?</span>)}
               </button>
 
               <div className="w-px h-6 bg-zinc-800" />
 
-              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" >
+              <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" >
                 Confirm trip
                 <ArrowRight className="size-5" />
               </button>
@@ -121,29 +134,6 @@ export function App() {
             </a>
           </p>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         {isGuestsModalOpen && (
           <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
@@ -189,6 +179,53 @@ export function App() {
                 </div>
 
                 <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Invite
+                  <Plus className="size-5" />
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+
+        {isConfirmTripModalOpen && (
+          <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <h2 className="font-lg font-semibold">Confirm trip creation</h2>
+                  <button>
+                    <X className="size-5 text-zinc-400" onClick={closeConfirmTripModal} />
+                  </button>
+                </div>
+
+                <p className="text-sm text-zinc-400">
+                  To complete the creation of the trip to <span className="font-semibold text-zinc-100">Florianópolis, Brazil</span> on the dates of <span className="font-semibold text-zinc-100">August 16 to 27, 2024</span> fill in your details below:
+                </p>
+              </div>
+
+
+              <form onSubmit={addNewEmailToInvite} className="space-y-3">
+                <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <User className="text-zinc-400 size-5" />
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Your fullname"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <User className="text-zinc-400 size-5" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Your email"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                   Invite
                   <Plus className="size-5" />
                 </button>
