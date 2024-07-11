72b0d2d1340feee19186394f1f92240b872b484c
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-07-11 11:54:28 -0300

    chore: My perfeccionism

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -100,7 +100,7 @@ export function TripDetailsPage() {
               Register new link
             </button>
           </div>
-          
+
 
           <div className="w-full h-px bg-zinc-800" />
 
@@ -129,7 +129,6 @@ export function TripDetailsPage() {
                 </div>
                 <CheckCircle2 className="text-zinc-400 size-5 shrink-0" />
               </div>
-
             </div>
 
             <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
