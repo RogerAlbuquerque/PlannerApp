893be1f45a87ebb362f8628afca4e4082e6f98f4
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-10-15 00:42:15 -0300

    feat:BACKEND Again reupdate project folders

--- frontend/src/pages/trip-details/activities.tsx
+++ frontend/src/pages/trip-details/activities.tsx
@@ -3,7 +3,7 @@ import { api } from "../../lib/axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
+import { enUS } from "date-fns/locale";
 
 interface Activity {
     date: string;
@@ -28,8 +28,8 @@ export function Activities() {
                 return (
                     <div key={category.date} className="space-y-2.5">
                         <div className="flex gap-2 items-baseline">
-                            <span className="text-xl text-zinc-300 font-semibold">Dia {format(category.date, 'd')}</span>
-                            <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: ptBR })}</span>
+                            <span className="text-xl text-zinc-300 font-semibold">day {format(category.date, 'd')}</span>
+                            <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: enUS })}</span>
                         </div>
                         {category.activities.length > 0 ? (
                             <div>
