c0502f6d5604a6fe7839fc470041519b8059e2d3
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-10-18 13:02:20 -0300

    feat:Create base codes to start first creation of new data on sqlite database

---
+++ backend/src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register;
+
+public class RegisterTripUseCase
+{
+    public void Execute(RequestRegisterTripJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestRegisterTripJson request)
+    {
+
+    }
+
+}
