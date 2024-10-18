cccfd0e0a4e5b22669fa2499d98f08794797c47d
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-10-18 13:19:53 -0300

    feat:Created a exception for wrong parameter in request for register new trip

--- backend/src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
+++ backend/src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -11,7 +11,20 @@ public class RegisterTripUseCase
 
     private void Validate(RequestRegisterTripJson request)
     {
+        if (string.IsNullOrWhiteSpace(request.Name))
+        {
+            throw new ArgumentException("Name cannot be empty");
+        }
 
+        if (request.StartDate < DateTime.UtcNow.Date)
+        {
+            throw new ArgumentException("Trip cannot be booked for a past date");
+        }
+
+        if (request.EndDate.Date <= request.StartDate.Date)
+        {
+            throw new ArgumentException("The trip must end after the start date");
+        }
     }
 
 }
